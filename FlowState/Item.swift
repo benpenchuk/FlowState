9b63766880b6786831da99fe26394b490cda6894
Ben Penchuk <ben@penchuk.com>
2026-01-13 20:43:12 -0500

    Initial Commit

---
+++ FlowState/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FlowState
+//
+//  Created by Ben Penchuk on 1/13/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FlowStateTests/FlowStateTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlowStateTests.swift
+//  FlowStateTests
+//
+//  Created by Ben Penchuk on 1/13/26.
+//
+
+import Testing
+
+struct FlowStateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
